6cbfff7563c9b06bd5e96ed2001fe6eceb350987
Lin1031 <44975556+Lin1031@users.noreply.github.com>
2021-11-26 18:51:01 +0800

    Update izhicheng.py

--- Spider/izhicheng.py
+++ Spider/izhicheng.py
@@ -7,6 +7,7 @@
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options  # 无GUI
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import os
 
@@ -37,7 +38,11 @@ def tianbiao(stuID):
     chrome_options.add_argument('no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
     chromedriver = "/usr/bin/chromedriver"
     os.environ["webdriver.chrome.driver"] = chromedriver
-    driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
+    # driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
+    driver = webdriver.Chrome(
+            executable_path=ChromeDriverManager().install(),
+            chrome_options=chrome_options,
+            service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
 
     try:
         # 表单地址
